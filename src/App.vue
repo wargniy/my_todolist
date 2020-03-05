47db37b9566a6e15db4cfbfacd37b734bf865a22
Reingraw <y.wargnier@gmail.com>
2020-03-05 01:23:55 +0100

    first commit, vue js to do list

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <ToDoList/>
+  </div>
+</template>
+
+<script>
+import ToDoList from './components/ToDoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ToDoList
+  }
+}
+</script>
+
+<style lang="scss">
+@import url('https://use.fontawesome.com/releases/v5.7.2/css/all.css');
+
+body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  background: linear-gradient(to bottom right,#EABFCB, #A4508B);
+}
+
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 1rem;
+  color: #190F16;
+  width: 100%;
+  max-width: 768px;
+}
+</style>
